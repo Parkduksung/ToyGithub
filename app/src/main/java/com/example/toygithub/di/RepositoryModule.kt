8ec68521648ac58f32310562ef1516bee6a0bce4
Parkduksung <duksung1234@naver.com>
2021-11-12 16:09:02 +0900

    [feature] di 추가.

--- app/src/main/java/com/example/toygithub/di/RepositoryModule.kt
+++ app/src/main/java/com/example/toygithub/di/RepositoryModule.kt
@@ -3,6 +3,8 @@ package com.example.toygithub.di
 
 import com.example.toygithub.data.repo.GithubRepository
 import com.example.toygithub.data.repo.GithubRepositoryImpl
+import com.example.toygithub.data.source.local.GithubLocalDataSource
+import com.example.toygithub.data.source.local.GithubLocalDataSourceImpl
 import com.example.toygithub.data.source.remote.GithubRemoteDataSource
 import com.example.toygithub.data.source.remote.GithubRemoteDataSourceImpl
 import dagger.Binds
@@ -22,6 +24,11 @@ abstract class RepositoryModule {
     @Singleton
     @Binds
     abstract fun bindGithubRemoteDataSource(githubRemoteDataSourceImpl: GithubRemoteDataSourceImpl): GithubRemoteDataSource
+
+    @Singleton
+    @Binds
+    abstract fun bindGithubLocalDataSource(githubLocalDataSourceImpl: GithubLocalDataSourceImpl): GithubLocalDataSource
+
 
 }
 
