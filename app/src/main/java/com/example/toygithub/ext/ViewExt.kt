a1caa8e8233fb7681cd2862498e89e1eeb14ac2f
Parkduksung <duksung1234@naver.com>
2021-11-15 16:08:31 +0900

    [feature] bindingAdapter 추가.

--- app/src/main/java/com/example/toygithub/ext/ViewExt.kt
+++ app/src/main/java/com/example/toygithub/ext/ViewExt.kt
@@ -1,9 +1,12 @@
 package com.example.toygithub.ext
 
 import android.content.Context
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.BindingAdapter
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 
 fun AppCompatActivity.showToast(context: Context = this, message: String) {
     Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
@@ -11,4 +14,13 @@ fun AppCompatActivity.showToast(context: Context = this, message: String) {
 
 fun Fragment.showToast(context: Context = this.requireContext(), message: String) {
     Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+}
+
+
+@BindingAdapter("bind:setUrlImg")
+fun ImageView.setUrlImg(url: String?) {
+    Glide.with(context)
+        .load(url)
+        .centerCrop()
+        .into(this)
 }
