3f917af50d450041f2e640a2ffc5412f5eaf6bd6
Parkduksung <duksung1234@naver.com>
2022-04-08 23:34:05 +0900

    [feature] user repos 추가.

--- app/src/main/java/com/example/toygithub/api/GithubApi.kt
+++ app/src/main/java/com/example/toygithub/api/GithubApi.kt
@@ -1,8 +1,10 @@
 package com.example.toygithub.api
 
+import com.example.toygithub.api.response.GithubRepoResponse
 import com.example.toygithub.api.response.GithubSearchResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface GithubApi {
@@ -14,4 +16,10 @@ interface GithubApi {
         @Query("per_page") perPage: Int = 100,
         @Query("page") page: Int = 0,
     ): Call<GithubSearchResponse>
+
+
+    @GET("users/{userId}/repos")
+    fun getUserRepos(
+        @Path("userId") userId: String
+    ) : Call<GithubRepoResponse>
 }
